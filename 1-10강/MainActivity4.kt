72a01d7634e1c0aea5ef1168b27629831bfa0ec5
22sonamu <22sonamu@naver.com>
2020-12-22 03:08:56 +0900

    val/var, for문, if/else문 -> Log.d를 통해 확인

---
+++ 1-10강/MainActivity4.kt
@@ -0,0 +1,37 @@
+package com.jungwha.sayhello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var myname = "여정화"
+        Log.d(TAG, "my name=$myname")
+        myname = "여정화2"
+        Log.d(TAG, "my name=$myname")
+        val myAge = 19
+        Log.d(TAG, "pi=$myAge")
+
+        var numbers = "1,2,3,4,5,6"
+        var thisWeekNumbers = "5,6,7,8,9,10"
+        if (numbers == thisWeekNumbers){
+            Log.d(TAG, "당첨되었습니다.")
+        }else{
+            Log.d(TAG, "당첨되지 않았습니다.")
+        }
+
+        for(idx in 1..10){
+            Log.d(TAG, "현재 숫자는 ${idx} 입니다.")
+
+        }
+
+
+    }
+}
